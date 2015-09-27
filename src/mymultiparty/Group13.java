b85becb007675489b6878f5b2f89d01a9cd954fb
thijser <thijsboumans@online.nl>
2015-09-27 15:38:31 +0200

    least painfull v1

--- src/mymultiparty/Group13.java
+++ src/mymultiparty/Group13.java
@@ -2,12 +2,14 @@ package mymultiparty;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import negotiator.Bid;
 
+import negotiator.Bid;
 import negotiator.actions.Accept;
 import negotiator.actions.Action;
 import negotiator.actions.Offer;
@@ -28,7 +30,8 @@ public class Group13 extends AbstractNegotiationParty {
 
     private double minUtility = 0.8;
     private double lastBid = 0;
-
+    private ArrayList<Offer> oldOffers = new ArrayList<Offer>();
+    Bid OurLast;
     public void init() {
         minUtility = Math.max(minUtility, utilitySpace.getReservationValueUndiscounted());
     }
@@ -57,18 +60,45 @@ public class Group13 extends AbstractNegotiationParty {
     }
 
     /**
+     * concedes the least painfull point
+     * @param ourLast	offer we currently have
+     * @param currentOther offer the opponent current has
+     * @return a compromise
+     */
+    public Bid generateLeastPainfullCompromise(Bid ourLast, Offer currentOther){
+    	Bid best=currentOther.getBid();
+    	int index=0;
+    	Iterator<Entry<Integer, Value>> it = currentOther.getBid().getValues().entrySet().iterator();
+    	HashMap<Integer,Value> ours = ourLast.getValues();
+    	while(it.hasNext()){
+    		Entry<Integer,Value> issue=it.next();
+    		if(!issue.getValue().equals(ours.get(issue.getKey()))){
+    			Bid compromise = new Bid(ourLast);
+    			compromise.setValue(issue.getKey(), issue.getValue());
+    			if(getUtility(compromise)>getUtility(best)){
+    				best=compromise;
+    			}
+    		}
+    	}
+		return best;
+    	
+    	
+    	
+    }
+    /**
      * All offers proposed by the other parties will be received as a message.
      * You can use this information to your advantage, for example to predict
      * their utility.
      *
      * @param sender The party that did the action.
      * @param action The action that party did.
-     */
+     **/
     @Override
     public void receiveMessage(Object sender, Action action) {
         super.receiveMessage(sender, action);
 
         if (action instanceof Offer) {
+        	oldOffers.add((Offer)action);
             lastBid = getUtility(((Offer) action).getBid());
         }
     }
@@ -78,7 +108,9 @@ public class Group13 extends AbstractNegotiationParty {
     }
 
     private Bid generateBid() throws Exception {
-        return utilitySpace.getMaxUtilityBid();
+    	if(oldOffers.size()==0)
+    		return OurLast=utilitySpace.getMaxUtilityBid();
+    	return OurLast=generateLeastPainfullCompromise(OurLast,oldOffers.get(oldOffers.size()-1));
     }
 
     @Override
