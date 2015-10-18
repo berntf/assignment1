562c2578d0b2776a7a7d023a095ad4d3a5ded641
Jorik0 <j.oostenbrink@student.tudelft.nl>
2015-10-18 13:59:18 +0200

    Generate all bids (only discrete and done recursively)

--- src/mymultiparty/USBNAT.java
+++ src/mymultiparty/USBNAT.java
@@ -1,10 +1,20 @@
 package mymultiparty;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import static mymultiparty.Group13.getAllBids;
+import negotiator.Bid;
 import negotiator.actions.Accept;
 import negotiator.actions.Action;
 import negotiator.actions.Offer;
+import negotiator.issue.ISSUETYPE;
+import negotiator.issue.Issue;
+import negotiator.issue.IssueDiscrete;
+import negotiator.issue.Value;
+import negotiator.issue.ValueDiscrete;
 import negotiator.parties.AbstractNegotiationParty;
 
 
@@ -15,6 +25,7 @@ import negotiator.parties.AbstractNegotiationParty;
 public class USBNAT extends AbstractNegotiationParty {
     
     HashMap<Object,FrequencyOpponentModel> opponents = new HashMap<>();
+    HashMap<Object,ArrayList<Bid>> accepts;
     double n = 0.1;
     
     
@@ -32,9 +43,72 @@ public class USBNAT extends AbstractNegotiationParty {
     	}
     	return 1;
     }
+    
+    private ArrayList<Bid> generateAllBids() throws Exception {
+        ArrayList<Issue> issues = utilitySpace.getDomain().getIssues();
+        
+        ArrayList<Bid> ret = new ArrayList();
+        
+        for (HashMap<Integer,Value> values : getAllBids(issues, 0)) {
+            Bid bid = new Bid(utilitySpace.getDomain(), values);
+            ret.add(bid);
+        }
+        
+        Collections.sort(ret, new Comparator<Bid>() {
+            @Override
+            public int compare(Bid a, Bid b) {
+                if (getUtility(b) > getUtility(a)) {
+                    return 1;
+                } else if(getUtility(b) == getUtility(a)) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            }
+            
+        });
+        
+        return ret;
+    }
+    
+    public static ArrayList<HashMap<Integer,Value>> getAllBids(ArrayList<Issue> issues, int from) throws Exception {        
+        Issue issue = issues.get(from);
+        
+        if (issue.getType() != ISSUETYPE.DISCRETE) {
+            throw new Exception("Issuetype " + issue.getType() + " not supported");
+        }
+        IssueDiscrete issueD = (IssueDiscrete)issue;
+        
+        
+        ArrayList<HashMap<Integer,Value>> bids;
+        
+        if (from == issues.size()-1) {
+            bids = new ArrayList();
+            bids.add(new HashMap());
+        } else {
+            bids = getAllBids(issues, from+1);
+        }
+        
+        ArrayList<HashMap<Integer,Value>> ret = new ArrayList();
+        
+        for (ValueDiscrete v : issueD.getValues()) {
+            for (HashMap<Integer,Value> bid : bids) {
+                HashMap<Integer,Value> newBid = new HashMap(bid);
+                newBid.put(issueD.getNumber(), v);
+                ret.add(newBid);
+            }
+        }
+        
+        return ret;
+    }
+    
     @Override
     public Action chooseAction(List<Class<? extends Action>> list) {
         try {
+            
+            System.out.println(getUtilityPerFraction(getTimeLine().getTime()));
+            System.out.println("-+-+-");
+            
             return new Offer(getUtilitySpace().getMaxUtilityBid());
         } catch (Exception ex) {
             System.err.println("Exception in chooseAction: " + ex.getMessage());
@@ -57,7 +131,6 @@ public class USBNAT extends AbstractNegotiationParty {
             } catch (Exception ex) {
                 System.err.println("Exception in receiveMessage: " + ex.getMessage());
             }
-            System.out.println(OM);
         }
     }
     
